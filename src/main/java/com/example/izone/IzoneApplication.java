a4006fae8836d7ccf3849dfe69e67bd72cf8169f
ujin216 <ujin3446@gmail.com>
2025-05-28 19:51:35 +0900

    Initial commit

---
+++ src/main/java/com/example/izone/IzoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.izone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IzoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IzoneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/izone/IzoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.izone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IzoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
