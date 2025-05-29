35f0646510f24dbd32b4e6f2c4927d6e47b72719
dogyugyeong <dhm1458@gmail.com>
2025-05-30 00:43:37 +0900

    feat: UserController 코드 작성

--- src/main/java/com/example/izone/domain/user/controller/UserController.java
+++ src/main/java/com/example/izone/domain/user/controller/UserController.java
@@ -1,12 +1,21 @@
 package com.example.izone.domain.user.controller;
 
+import com.example.izone.domain.user.dto.request.SignupRequest;
 import com.example.izone.domain.user.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/users")
-
+@RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
 
-
+@PostMapping("/signup")
+public ResponseEntity<Void> signup(@RequestBody @Valid SignupRequest request) {
+    userService.signup(request.getEmail(),
+            request.getPassword(), request.getName(), request.getIntro(), request.getAge());
+        return ResponseEntity.status(HttpStatus.OK).build();}
 }
