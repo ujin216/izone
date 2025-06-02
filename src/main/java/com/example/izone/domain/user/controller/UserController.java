986f5d0820f06600aed4b23c831ec6c3805ffc98
dogyugyeong <dhm1458@gmail.com>
2025-06-02 09:37:48 +0900

    feat: UserController login,delete 코드 추가

--- src/main/java/com/example/izone/domain/user/controller/UserController.java
+++ src/main/java/com/example/izone/domain/user/controller/UserController.java
@@ -1,6 +1,10 @@
 package com.example.izone.domain.user.controller;
 
+import com.example.izone.domain.entitiy.User;
+import com.example.izone.domain.user.dto.request.DeleteRequest;
+import com.example.izone.domain.user.dto.request.LoginRequest;
 import com.example.izone.domain.user.dto.request.SignupRequest;
+import com.example.izone.domain.user.dto.respone.UserResponse;
 import com.example.izone.domain.user.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -19,4 +23,22 @@ public class UserController {
         userService.signup(signupRequest);
         return ResponseEntity.status(HttpStatus.OK).body("회원가입 완료되었습니다");
     }
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody @Valid LoginRequest loginRequest){
+        User user = userService.login(loginRequest.getEmail(), loginRequest.getPassword());
+        UserResponse userResponse = new UserResponse(user);
+        return ResponseEntity.status(HttpStatus.OK).body("로그인이 완료되었습니다");
+
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<String> delete(@RequestBody @Valid DeleteRequest deleteRequest) {
+        userService.delete(deleteRequest.getEmail(), deleteRequest.getPassword());
+        return ResponseEntity.status(HttpStatus.OK).body("회원탈퇴가 완료되었습니다");
+
+
+
+
+}
+
 }
